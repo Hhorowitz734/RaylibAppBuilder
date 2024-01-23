9f62e6c077744817b83246fb55ed4de36671caec
Benjamin_Horowitz <hhorowitz734@gmail.com>
2024-01-23 17:54:40 -0600

    documented lexer functions

--- include/lexer.h
+++ include/lexer.h
@@ -25,13 +25,71 @@ typedef struct Token {
     struct Token* next;
 } Token;
 
+/**
+ * @brief Opens a file given its filename
+ * 
+ * @note Used as a utility function to read whatever file we are parsing
+ * 
+ * @param filename String name of the file to open
+*/
 FILE* openFile(const char* filename);
+
+/**
+ * @brief Lexes the file, breaks it down into tokens to be further processed by parser
+ * 
+ * @param file The file object, obtained using openFile utility function
+ * 
+ * @returns The head token of the token stream
+ * @note Token stream operates as a linked list, with end token having type END
+*/
 Token* parseFile(FILE* file);
 
+/**
+ * @brief Processes a <> tag for an element
+ * 
+ * @param input The input string (line of a file provided by parseFile function)
+ * @param position Pointer to int holding position in line
+ * 
+ * @returns The Token* of the tag
+ * 
+ * @note Used as a utility function for parseFile
+*/
 Token* processTag(char* input, int* position);
+
+/**
+ * @brief Returns the token type of a Token given its lexeme
+ * 
+ * @param lexeme The lexeme to get the type of
+ * 
+ * @returns The TType that should be assigned to a token with that lexeme
+ * 
+ * @note Potentially will be renamed to tagSwitch in a future version of this program
+ * @note Used as a utility function for processTag
+*/
 TType typeSwitch(char* lexeme);
 
+/**
+ * @brief Processes a directive tag like SET as part of the head
+ * 
+ * @param input The input string (line of a file provided by parseFile function)
+ * @param position Pointer to int holding position in line
+ * 
+ * @returns The Token* pointer to the directive's token
+ * 
+ * @note Used as a utility function for parseFile
+*/
 Token* processDirective(char* input, int* position);
+
+/**
+ * @brief Returns the token type of a directive Token given its lexeme
+ * 
+ * @param lexeme The lexeme to get the type of
+ * 
+ * @returns The TType that should be assigned to a token with that lexeme
+ * 
+ * @note Potentially will be renamed to tagSwitch in a future version of this program
+ * @note Used as a utility function for processTag
+*/
 TType directiveSwitch(char* lexeme);
 
 #endif
