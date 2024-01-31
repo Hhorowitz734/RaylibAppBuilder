fc3b0a32eba416dfb59ef9e1a16a6623d6e2fd7e
Benjamin_Horowitz <hhorowitz734@gmail.com>
2024-01-31 00:29:07 -0600

    created parser for box settings

--- include/components/box.h
+++ include/components/box.h
@@ -4,10 +4,14 @@
 #include "raylib.h"
 #include "../lexer.h"
 #include <stdio.h>
+#include <string.h>
 
 typedef struct Box {
-    float width;
-    float height;
+
+    char* name; 
+
+    int width;
+    int height;
 
     float borderWidth;
     Color borderColor;
@@ -22,6 +26,8 @@ typedef struct Box {
 void setBoxSettings(Box* box, char* token_lexeme);
 
 Box* createBox(Token* boxToken);
+
+void switchSettings(Box* box, char* setting, int value);
 
 
 
--- src/components/box.c
+++ src/components/box.c
@@ -11,10 +11,57 @@ Box* createBox(Token* boxToken) {
     
 }
 
-void setBoxSettings(Box* box, char* token_lexeme) {
+void setBoxSettings(Box* box, char* tokenLexeme) {
 
     int position = 0;
+    int initial_position = 0;
 
-    printf("HERE\n%s\n\n\n\n\n", token_lexeme);
+    while (tokenLexeme[position] != ' ') { position++; }
+
+    while (tokenLexeme[position] != '\0') { 
+
+        initial_position = position;
+        
+        while (tokenLexeme[position] != '=') { position++; }
+
+        char* setting = malloc(position - initial_position + 1);
+        strncpy(setting, &tokenLexeme[initial_position + 1], position - initial_position);
+
+        setting[position - initial_position] = '\0';
+
+        while (tokenLexeme[position] == ' ' || tokenLexeme[position] == '=') { position++; }
+
+        initial_position = position;
+
+        while (tokenLexeme[position] != ' ') { position++; }
+
+        char* value = malloc(position - initial_position + 1);
+        strncpy(value, &tokenLexeme[initial_position], position - initial_position);
+        value[position - initial_position] = '\0';
+
+        int i_value = atoi(value);
+
+        switchSettings(box, setting, i_value);
+
+        while (tokenLexeme[position] != '\0' && tokenLexeme[position] != '|') { position++; }
+
+
+        
+    }
+
+}
+
+void switchSettings(Box* box, char* setting, int value) {
+
+    if (strncmp(setting, "WIDTH", 5) == 0) { 
+        box->width = value;
+        printf("[Box Parser]: Set box width to: %d\n", box->width); }
+    else if (strncmp(setting, " HEIGHT", 7) == 0) {
+        box->height = value;
+        printf("[Box Parser]: Set box height to: %d\n", box->height);
+    }
+    else {
+        printf("%s\n", setting);
+    }
 
 }
